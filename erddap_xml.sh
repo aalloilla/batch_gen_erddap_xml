24c595f428a8f2aa39344d7bf75a3e3fa99ad68d
Tuomo S <tuosaa@gmail.com>
2024-05-16 16:04:51 -0500

    Added capability to loop through folders.

--- erddap_xml.sh
+++ erddap_xml.sh
@@ -1,35 +1,73 @@
 #!/bin/bash
 #
-#---script to generate xml files for all the nc files in a directory.
-#---indir = input directory
-#---outdir = output directory
-#---outfile= output file containing all the xml files
+#---Script to generate ERDDAP dataset xml files for all the .nc files in directories under <rootdir>.
+#---Basename of the directories need to be gives as parameters to the script.
+#---Folders need to be named as <dirbasename>_<indir_postfix>. 
+#---Outputs go to directories named <dirbasename>_<outdir_postix>
+#---outfile= output file containing all the ERDDAP xml files. 
+# 
+#  This script suits a situation where one needs to generate the XM Lsnippet for each .nc file separately.
 
-indir=/Users/mstoessel/GOMRI/MBON/Walton/Walton-Smith/WS22141_Profile/
-outdir=/Library/erddap_data/erddap/logs/
-outfile=GenerateDatasetsXml_WS22141.out
+### ~/Desktop/WinLin_Share/00-GCOOS/00-MBON/scripts/generate_dataset_xml
 
-printf "<!--  Start WS22141 --> \n \n \n" > $outdir$outfile
+#-------------------------------------------------------------
+# USER DEFINED DIRECTORIES FOR DATA AND ERDDAP BIGPARENTDIR
+#-------------------------------------------------------------
+# Data directory (data):
+#rootdir=/home/tsaari-admin/Desktop/WinLin_Share/00-GCOOS/00-MBON/WaltonSmith/
+rootdir=/data/erddap/Walton-Smith/
+# ERDDAP GeneratDatasetsXml script location
+erddap_scriptdir=/usr/local/apache-tomcat-10.1.7/webapps/erddap/WEB-INF/
+# ERDDAP output directory for the generated XML files:
+xmldir=/usr/local/erddap_bigparentdir/logs/
 
-cd $indir
 
-for ff in `ls *.nc`
+#outfile=GenerateDatasetsXml_WS22141.out
 
+# NOTE: Bash command to rename folders (_nc suffic to _Profile suffix):
+# for adir in `ls -d *nc`; do basename="${adir/%_nc}"; sudo mv $adir "${basename}_Profile";  done
+
+# data directories to be looped through are to be given as cmd line arguments
+
+for adir in $@
 do
-	echo $ff
+    # the XML otput file for the current folder (e.g. a cruise in case of Walton-Smith CTD data)
+    outfile=GenerateDatasetsXml_${adir}.out
+    # the current data input folder
+    indir="${rootdir}${adir}/"
 
-        cd /Library/Tomcat/apache-tomcat-9.0.54/webapps/erddap/WEB-INF/
-        GenerateDatasetsXml.sh  EDDTableFromNcCFFiles $indir $ff $indir$ff 80 "" "" "" "" "" "" "" "" "" "" ""
+    # start the loop thru files
+    printf "<!--  Start $adir --> \n \n \n" > $xmldir$outfile
+    # change to the directory and loop through .nc files
+    cd $indir
+    for ff in `ls *.nc`
+    do
+        echo $ff
+        # change to erddap dir and run the XML generation script
+        cd $erddap_scriptdir
+        echo "CURRENT DIR:"
+        pwd
+        ${erddap_scriptdir}GenerateDatasetsXml.sh  EDDTableFromNcCFFiles $indir $ff $indir$ff 180 "" "" "" "" "" "" "" "" "" "" ""
 
-        filename=$(sed -n 's/<fileNameRegex>\([^<]*\).nc.*/\1/p' ${outdir}GenerateDatasetsXml.out)
-
+	# get the filename fron the XML output
+        filename=$(sed -n 's/<fileNameRegex>\([^<]*\).nc.*/\1/p' ${xmldir}GenerateDatasetsXml.out)
+        echo "FILE NAME IN XML OUTPUT: ___  $filename   ___"
+	# replace dot with underscore (ERDDAP doesn't allow dots in datasetID)
         filename=$(echo $filename | sed 's/\./_/g' )
+	# replace dash with underscore (ERDDAP doesn't allow dashes in datasetID)
+        filename=$(echo $filename | sed 's/-/_/g' )
 
-        sed -i"old" "s/datasetID=\"[^\"]*\"/datasetID=\"${filename}\"/g" ${outdir}GenerateDatasetsXml.out
+	# replace the datasetID in the XML output with the filename generated above
+	sed -i "s/datasetID=\"[^\"]*\"/datasetID=\"${filename}\"/g" ${xmldir}GenerateDatasetsXml.out
+	# Mac needs -i flag
+	#sed -i"old" "s/datasetID=\"[^\"]*\"/datasetID=\"${filename}\"/g" ${xmldir}GenerateDatasetsXml.out
+        #/bin/rm ${xmldir}GenerateDatasetsXml.xmlold
 
-        /bin/rm ${outdir}GenerateDatasetsXml.xmlold
+	cat  ${xmldir}GenerateDatasetsXml.out >> $xmldir$outfile
+    done
 
-	cat  ${outdir}GenerateDatasetsXml.out >> $outdir$outfile
+    printf "<!--  End $adir --> \n" >>  $xmldir$outfile
+    cd ..
+
 done
 
-printf "<!--  End WS22141 --> \n" >>  $outdir$outfile
